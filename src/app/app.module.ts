ff9a3c14b0976a76d3b6b9a86c73aa77d3a7db58
Andre <amgowano.apm@gmail.com>
2019-12-23 09:28:13 +0300

    feat(package.json): Update angular version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HrisMenuModule } from '@iapps/hris-menu';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { RoutingModule } from '../app/app.routes';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store/reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { effects } from './store/effects';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HrisMenuModule } from "@iapps/hris-menu";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./store/reducers";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
+import { EffectsModule } from "@ngrx/effects";
+import { effects } from "./store/effects";
+import { routes, RoutingModule } from "./app.routes";
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -19,9 +21,13 @@ import { effects } from './store/effects';
     BrowserAnimationsModule,
     HttpClientModule,
     RoutingModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    EffectsModule.forRoot(effects)
+    EffectsModule.forRoot(effects),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
